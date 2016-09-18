f22250f3b60a0d0ca17d99e1a98a92c1b78b5e62
user01 <user01@mac-mini-user01.local>
2016-09-18 07:08:53 +0300

    Initial Commit

---
+++ PinterestPreview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PinterestPreview
+//
+//  Created by user01 on 18.09.16.
+//  Copyright © 2016 temrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PinterestPreview/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PinterestPreview
+//
+//  Created by user01 on 18.09.16.
+//  Copyright © 2016 temrov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
